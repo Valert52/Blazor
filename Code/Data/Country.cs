cdcf2c5b86f6aad13ca5cbf7c84d2a7e5930b3b7
Valeria Turtšina <valert@taltech.ee>
2026-03-05 13:42:22 +0200

    added classes Currency and Country

---
+++ Code/Data/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace Abc.Data;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string IsoCode { get; set; }
+    public string Capital { get; set; }
+}
